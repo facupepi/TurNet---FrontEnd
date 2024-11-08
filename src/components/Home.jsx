a6c57bac124185505e9b09c27fee1ea659604b8e
Santiago Villalba <santiagovillalba-2002@hotmail.com>
2024-11-08 20:18:57 -0300

    estilos para home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,54 +4,28 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 const Home = () => {
   return (
     <div>
-      <div className='bg-breadcrumb'></div>
-      
-        <Container>
-          <h1>Bienvenido a Mi Aplicación</h1>
-          <p>
-            Una solución ágil y sencilla para la gestión de turnos, diseñada para empresas y emprendimientos.
-          </p>
-          <Button variant="primary" href="/register">Regístrate</Button>
-        </Container>
-      
-      <Container className="mt-5">
-        <Row>
-          <Col md={4}>
-            <Card className="mb-4">
-              <Card.Img variant="top" src="https://via.placeholder.com/150" />
-              <Card.Body>
-                <Card.Title>Consulta de Disponibilidad</Card.Title>
-                <Card.Text>
-                  Consulta la disponibilidad del servicio y selecciona el horario que desees.
-                </Card.Text>
-                <Button variant="primary" href="/services">Ver Servicios</Button>
-              </Card.Body>
-            </Card>
+      <div className='bg-breadcrumb'>
+          <h1>TURNET</h1>
+      </div>
+      <Container className="mt-5 mb-5">
+        <Row className='d-flex align-items-center justify-content-center'>
+        
+          <Col md={8}>
+          <h2 className='mt-5 mb-5'>Conoce todos los servicios disponibles</h2>
+          <div className='card-service'>
+            <img src="https://via.placeholder.com/200" alt="service" className='card-service-img' />
+            <div className='card-service-body'>
+              <h5 className='font-weight-bold'>Service 1</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non diam commodo felis blandit placerat. 
+                Nullam lacinia placerat egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <Button variant="primary" href='/reservar'>Reservar</Button>
+            </div>
+
+          </div>
+            
           </Col>
-          <Col md={4}>
-            <Card className="mb-4">
-              <Card.Img variant="top" src="https://via.placeholder.com/150" />
-              <Card.Body>
-                <Card.Title>Visualiza tus Turnos</Card.Title>
-                <Card.Text>
-                  Visualiza los turnos que has reservado y gestiona tus citas fácilmente.
-                </Card.Text>
-                <Button variant="primary" href="/bookings">Mis Turnos</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="mb-4">
-              <Card.Img variant="top" src="https://via.placeholder.com/150" />
-              <Card.Body>
-                <Card.Title>Acceso Administrador</Card.Title>
-                <Card.Text>
-                  Accede en tiempo real a los turnos reservados por los clientes.
-                </Card.Text>
-                <Button variant="primary" href="/admin">Administrar</Button>
-              </Card.Body>
-            </Card>
-          </Col>
+          
         </Row>
       </Container>
     </div>
